d9c1fa184decbd224991d3ede51751db6ed22e4b
Eugene Agesa <eugeneagesa734@gmail.com>
2023-07-17 15:23:53 -0700

    all possible combinations code corrected

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,25 +7,19 @@
  */
 int main(void)
 {
-	int i = 0;
+	int a, b;
 
-	while (i < 8)
+	for (a = 0; a <= 9; a++)
 	{
-		int j = i + 1;
-
-		while (j < 10)
+		for (b = a; b <= 9; b++)
 		{
-			putchar(i + '0');
+			putchar(a + '0');
 			putchar(',');
 			putchar(' ');
-			putchar(i + '0');
-			if (i == 8 && j == 9)
-				break;
+			putchar(b + '0');
 			putchar(',');
 			putchar(' ');
-			j++;
 		}
-		i++;
 	}
 	putchar('\n');
 	return (0);
