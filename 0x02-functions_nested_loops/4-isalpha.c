35309b4127b8a157fdadfd104c45c17d18eac2f7
Eugene Agesa <eugeneagesa734@gmail.com>
2023-07-19 03:46:31 -0700

    detect character c when in uppercase or lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _isalpha - return 1 when c is upper or lower
+ *
+ * Description: using the _putchar function
+ *
+ * @c: collects characters
+ *
+ * Return: 1 if c is low or cap otherwise 0
+ */
+int _isalpha(int c)
+{
+	char l, u;
+	int letter = '0';
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		for (u = 'A'; u <= 'Z'; u++)
+		{
+		if (l == c || u == C)
+		{
+			letter = 1;
+		}
+		}
+	}
+	return (letter);
+}
+
